0c49b6f2562aecffc9ff45ab81a37451bcdff341
acaty64 <ana.arashiro@gmail.com>
2017-11-09 06:22:20 +0000

    setup?phpunit2.sh

---
+++ ci/setup_phpunit2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+    
+    set -e
+# Set PHP version
+phpenv local 7.0
+
+# Remove XDebug
+rm -f /home/rof/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
+
+# Increase memory
+sed -i'' 's/^memory_limit=.*/memory_limit = 512m/g' ${HOME}/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# Install dependencies through Composer
+composer install --prefer-source --no-interaction
+
+# Seed database
+php artisan migrate:refresh --seed
+
+# Start the PHP server
+php artisan serve >/dev/null 2>&1 &
+
+vendor/bin/phpunit
