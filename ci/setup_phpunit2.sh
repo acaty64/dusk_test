310afb78d0b602b2c66d69435410fedcd4749566
acaty64 <ana.arashiro@gmail.com>
2017-11-09 06:32:44 +0000

    setup_phpunit2.sh

--- ci/setup_phpunit2.sh
+++ ci/setup_phpunit2.sh
@@ -10,6 +10,8 @@ rm -f /home/rof/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 # Increase memory
 sed -i'' 's/^memory_limit=.*/memory_limit = 512m/g' ${HOME}/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
+cp .env.codeship .env
+
 # Install dependencies through Composer
 composer install --prefer-source --no-interaction
 
