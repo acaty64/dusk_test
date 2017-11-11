d1c754f36b92b643216e091f25def062598c7d4b
acaty64 <ana.arashiro@gmail.com>
2017-11-11 18:46:07 +0000

    Phpunit Auser02 and BAccesos01, Dusk A02Accesos01

--- tests/Browser/A02Accesos01Test.php
+++ tests/Browser/A02Accesos01Test.php
@@ -17,22 +17,25 @@ class A02Accesos01Test extends DuskTestCase
         $this->artisan('db:seed');
         $this->browse(function(Browser $browser)
         {
-            $user = User::find(3);
-            $browser->loginAs(User::find(2))
+            $user_admin = User::find(2);      // 4: Docente a modificar
+            $user_mody = User::find(4);      // 4: Docente a modificar
+            $browser->loginAs($user_admin)    
                     ->visit('/home')
                     ->assertPathIs('/home')
                     ->select('facultad_id','1')
                     ->select('sede_id','1')
                     ->press('Acceder')
-                    ->pause(2500)
+                    ->pause(10000)
                     ->waitForText('Inicio')
                     ->assertSee('Usuarios')
                     ->visit('/administrador/user/index')
                     ->assertPathIs('/administrador/user/index')
-                    ->visit("/administrador/acceso/edit/{$user->id}")
+                    ->visit("/administrador/acceso/edit/{$user_mody->id}")
+                    ->pause(2500)
+                    ->waitForText('Modificar Accesos de Usuario ')
                     ->select('type_id',2)
                     ->press('Grabar modificaciones')
-                    ->assertSee('Se ha modificado el usuario: ' . $user->id . ' : ' . $user->datauser->wdoc2 . " " . $user->datauser->wdoc3 . ", " . $user->datauser->wdoc1 . ' de forma exitosa')
+                    ->assertSee('Se ha modificado el usuario: ' . $user_mody->id . ' : ' . $user_mody->datauser->wdoc2 . " " . $user_mody->datauser->wdoc3 . ", " . $user_mody->datauser->wdoc1 . ' de forma exitosa')
                     ;
 
         });
